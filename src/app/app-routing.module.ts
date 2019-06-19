439b8bfcd9ee3fd198c35fde4ff79df27c4929cf
Maximilian72 <maxdean@live.co.uk>
2019-06-19 21:55:08 +0100

    Getting stated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: 'splash', pathMatch: 'full' },
+  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
+  { path: 'splash', loadChildren: './pages/splash/splash.module#SplashPageModule' },
+  { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule' },
+  { path: 'template', loadChildren: './pages/template/template.module#TemplatePageModule' },
+  { path: 'create', loadChildren: './pages/create/create.module#CreatePageModule' },
 ];
 
 @NgModule({
