b9224c9cd564b83a93102ce098b0687c6ed38a1d
Max <maxdean@live.co.uk>
2019-10-20 14:59:56 +0100

    Text over images

--- src/app/pages/create/create.page.ts
+++ src/app/pages/create/create.page.ts
@@ -12,6 +12,13 @@ export class CreatePage implements OnInit {
   sourceArray = [];
 
   urlsIn: any;
+
+  upperLower = "Add bottom text"
+  lowerText = true;
+  text = null;
+  T1:string;
+  T2:string;
+  T3:string;
  
   constructor(private route: ActivatedRoute, private router: Router) {
 
@@ -24,7 +31,6 @@ export class CreatePage implements OnInit {
       }
 
       let noURLs = this.urlsIn.urls.length;
-
       for(let i = 0; i < noURLs; i++){
 
         this.sourceArray[i] = this.urlsIn.urls[i];
@@ -54,9 +60,88 @@ export class CreatePage implements OnInit {
       this.canvasArray[i].drawImage(image,0,0);
 
       // Testing TEXT
-      this.canvasArray[i].font = "15px Simpsons"
-      this.canvasArray[i].fillText("Noodleman", 10, 15)
+      // this.canvasArray[i].font = "15px Simpsons"
+      // this.canvasArray[i].fillText("Noodleman", 10, 15)
 
+    }
+
+  }
+
+  addText(i){
+
+    this.canvasArray[i].font = "25px Simpsons"
+
+    let inputElName = "T" + i.toString();
+    let inputEl = <HTMLInputElement>document.getElementById(inputElName);
+    let text = "" + inputEl.value;
+    console.log(text);
+    
+    // let maxWidth = this.canvasArray[i].width;
+    let maxWidth = 300
+    console.log("maxWidth: " + maxWidth)
+
+    let lineHeight = 50;
+    let x = 10;
+    let y = 50;
+
+    let words = text.split(" ");
+    let line = "";
+
+    let len = words.length;
+    for (let j = 0; j < len; j++){
+
+      let testLine = line + words[j] + " ";
+      let metrics = this.canvasArray[i].measureText(testLine);
+      let testWidth = metrics.width;
+      console.log("metrics: " + metrics)
+      console.log("testLine: " + testLine)
+      console.log("testWidth: " + testWidth);
+      console.log("maxWidth: " + maxWidth);
+
+      if(testWidth > maxWidth){
+
+        this.canvasArray[i].fillText(line, x, y);
+        line = words[j] + "";
+        y += lineHeight;
+
+      } else {
+
+        line = testLine;
+
+      }
+
+      this.canvasArray[i].fillText(line, x, y);
+
+    }    
+
+  }
+
+  switchUpperLower(){
+
+    if(this.lowerText){
+      this.lowerText = false;
+      this.upperLower = "Add top text"
+    } else {
+      this.lowerText = true;
+      this.upperLower = "Add bottom text"
+    }
+
+  }
+
+  upperButton(){
+
+    if(this.lowerText){
+      console.log("lower text selected");
+      this.switchUpperLower();
+    }
+
+  }
+
+  lowerButton(){
+
+    if(!this.lowerText){
+      console.log("upper text selected");
+      this.switchUpperLower();
     }
 
   }
